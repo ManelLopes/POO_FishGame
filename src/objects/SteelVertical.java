70c86ba94902a94e48b046594e6e1e40c8feeb30
ManelLopes <mmrls1@iscte-iul.pt>
2025-12-02 13:51:36 +0000

    SteelVertical adicionado

---
+++ src/objects/SteelVertical.java
@@ -0,0 +1,33 @@
+package objects;
+
+import pt.iscte.poo.game.Room;
+
+public class SteelVertical extends GameObject {
+
+	public SteelVertical(Room room) {
+		super(room);
+	}
+
+	@Override
+	public String getName() {
+		return "steelVertical";
+	}
+
+	@Override
+	public int getLayer() {
+		return 1;
+	}
+
+	@Override
+	public boolean isMovable() {
+		return false;
+	}
+
+	@Override
+	public boolean hasGravity() {
+		return false;
+	}
+
+
+
+}
